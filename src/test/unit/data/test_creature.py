c1dad96a237e8218fad63404c153e7a54ca5e0a5
BlooAM <adam3200@vp.pl>
2024-11-11 16:25:39 +0100

    get endpoint tests

--- src/test/unit/data/test_creature.py
+++ src/test/unit/data/test_creature.py
@@ -27,3 +27,13 @@ def test_create(sample):
 def test_create_duplicate(sample):
     with pytest.raises(Duplicate):
         _ = creature.create(sample)
+
+
+def test_get_exists(sample):
+    resp = creature.get_one(sample.name)
+    assert resp == sample
+
+
+def test_get_missing():
+    with pytest.raises(Missing):
+        _ = creature.get_one("nonexisting")
