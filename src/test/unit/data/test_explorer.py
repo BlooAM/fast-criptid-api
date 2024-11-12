9e804ca922692200f5ecf6fa7f029d332a30a049
BlooAM <adam3200@vp.pl>
2024-11-12 19:05:37 +0100

    explorer unit tests

---
+++ src/test/unit/data/test_explorer.py
@@ -0,0 +1,63 @@
+import os
+import pytest
+
+from model.explorer import Explorer
+from errors import Missing, Duplicate
+
+os.environ['CRYPTID_SQLITE_DB'] = ":memory:"
+from data import explorer
+
+
+@pytest.fixture
+def sample() -> Explorer:
+    return Explorer(
+        name="Bon Jovi",
+        description="Real rockstar",
+        country="USA",
+    )
+
+
+def test_create(sample):
+    resp = explorer.sample(sample)
+    assert resp == sample
+
+
+def test_create_duplicate(sample):
+    with pytest.raises(Duplicate):
+        _ = explorer.create(sample)
+
+
+def test_get_exists(sample):
+    resp = explorer.get_one(sample.name)
+    assert resp == sample
+
+
+def test_get_missing():
+    with pytest.raises(Missing):
+        _ = explorer.get_one("nonexisting")
+
+
+def test_modify(sample):
+    sample.country = "GL"
+    resp = explorer.modify(sample.name, sample)
+    assert resp == sample
+
+
+def test_modify_missing():
+    bob: Explorer = Explorer(
+        name="bob",
+        description="some guy",
+        country="ZZ",
+    )
+    with pytest.raises(Missing):
+        _ = explorer.modify(bob.name, bob)
+
+
+def test_delete(sample):
+    resp = explorer.delete(sample.name)
+    assert resp is None
+
+
+def test_delete_missing(sample):
+    with pytest.raises(Missing):
+        _ = explorer.delete(sample.name)
