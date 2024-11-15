16a8e355b6f6b3be2c03ed1cdfe16de268cd50e1
BlooAM <adam3200@vp.pl>
2024-11-15 08:29:43 +0100

    modify/delete db ops

--- src/data/user.py
+++ src/data/user.py
@@ -25,3 +25,60 @@ def row_to_model(row: tuple) -> User:
 
 def model_to_dict(user: User) -> User:
     return user.dict()
+
+
+def get_one(name: str) -> User:
+    query = "select * from user where name=:name"
+    params = {'name': name}
+    curs.execute(query, params)
+    row = curs.fetchone()
+    if row:
+        return row_to_model(row)
+    else:
+        raise Missing(msg=f'User {name} not found')
+
+
+def get_all() -> list[User]:
+    query = "select * from user"
+    curs.execute(query)
+    return [row_to_model(row) for row in curs.fetchall()]
+
+
+def create(user: User, table: str = "user"):
+    query = f"""
+        insert into {table} (name, hash)
+        values (:name, :hash)
+    """
+    params = model_to_dict(user)
+    try:
+        curs.execute(query, params)
+    except IntegrityError:
+        raise Duplicate(msg=f'{table}: user {user.name} already exists')
+
+
+def modify(name: str, user: User) -> User:
+    query = """
+        update user
+        set name=:name, hash=:hash
+        where name=:name0
+    """
+    params = {
+        'name': user.name,
+        'hash': user.hash,
+        'name0': name,
+    }
+    curs.execute(query, params)
+    if curs.rowcount == 1:
+        return get_one(user.name)
+    else:
+        raise Missing(msg=f'User {name} not found')
+
+
+def delete(name: str) -> None:
+    user = get_one(name)
+    query = "delete from user where name=:name"
+    params = {'name': name}
+    curs.execute(query, params)
+    if curs.rowcount != 1:
+        raise Missing(msg=f'User {user} not found')
+    create(user, table='xuser')
