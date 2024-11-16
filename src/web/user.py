06091280c655f7cb3cc409a2807fa3007992d6cb
BlooAM <adam3200@vp.pl>
2024-11-16 19:56:35 +0100

    endpoint  user interface

--- src/web/user.py
+++ src/web/user.py
@@ -40,3 +40,38 @@ def get_access_token(token: str = Depends(oauth2_dep)) -> dict:
     return {'token': token}
 
 
+@router.get('/')
+def get_all() -> list[User]:
+    return service.get_all()
+
+
+@router.get('/{name}')
+def get_one(name) -> User:
+    try:
+        return service.get_one(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
+
+
+@router.post('/', status_code=201)
+def create(user: User) -> User:
+    try:
+        return service.create(user)
+    except Duplicate as e:
+        raise HTTPException(status_code=409, detail=e.msg)
+
+
+@router.patch('/')
+def modify(name: str, user: User) -> User:
+    try:
+        return service.modify(name, user)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
+
+
+@router.delete('/{name}')
+def delete(name: str) -> None:
+    try:
+        return service.delete(name)
+    except Missing as e:
+        raise HTTPException(status_code=404, detail=e.msg)
