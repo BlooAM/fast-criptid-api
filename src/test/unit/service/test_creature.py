205c91a56139fb8d5af55047fb3e3b5231e2487a
BlooAM <adam3200@vp.pl>
2024-11-23 10:24:38 +0100

    GET endpoint test for creature service layer

--- src/test/unit/service/test_creature.py
+++ src/test/unit/service/test_creature.py
@@ -1,26 +1,29 @@
+import os
+os.environ['CRYPTID_UNIT_TEST'] = "true"
+import pytest
+
 from model.creature import Creature
-from service import creature as code
+from service import creature as data
+from errors import Missing, Duplicate
 
 
-sample = Creature(
-    name='Yeti',
-    country='CN',
-    area='Himalayas',
-    description='Hirsute Himalayan',
-    aka='Abominable Snowman',
-)
+@pytest.fixture
+def sample() -> Creature:
+    return Creature(
+        name='Yeti',
+        country='CN',
+        area='Himalayas',
+        description='Hirsute Himalayan',
+        aka='Abominable Snowman',
+    )
 
 
-def test_create():
-    resp = code.create(sample)
+def test_create(sample):
+    sample.name = 'AnotherYeti'
+    resp = data.create(sample)
     assert resp == sample
 
 
-def test_get_exists():
-    resp = code.get_one('Yeti')
-    assert resp == sample
-
-
-def test_get_missing():
-    resp = code.get_one('boxturtle')
-    assert resp is None
+def test_create_dupplicate():
+    with pytest.raises(Duplicate):
+        resp = data.create(sample)
