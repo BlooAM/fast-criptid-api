ca4c68a9daa67d7d585584df36d2cb238ff9b589
BlooAM <adam3200@vp.pl>
2024-11-23 10:19:16 +0100

    adjust service creature for tests

--- src/service/creature.py
+++ src/service/creature.py
@@ -1,12 +1,16 @@
+import os
 from model.creature import Creature
-import data.creature as data
+if os.getenv('CRYPTID_UNIT_TEST'):
+    from fake import creature as data
+else:
+    from data import creature as data
 
 
 def get_all() -> list[Creature]:
     return data.get_all()
 
 
-def get_one(name: str) -> Creature | None:
+def get_one(name: str) -> Creature:
     return data.get_one(name)
 
 
@@ -14,13 +18,9 @@ def create(creature: Creature) -> Creature:
     return data.create(creature)
 
 
-def modify(id, creature: Creature) -> Creature:
-    return data.modify(id, creature)
+def modify(name: str, creature: Creature) -> Creature:
+    return data.modify(name, creature)
 
 
-def replace(id, creature: Creature) -> Creature:
-    return data.replace(id, creature)
-
-
-def delete(id, creature: Creature) -> bool:
-    return data.delete(id)
+def delete(name: str) -> None:
+    return data.delete(name)
